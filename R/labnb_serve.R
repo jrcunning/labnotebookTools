562beda29a0bf2cb335d43836046b2ab9b00cd98
Ross Cunning <ross.cunning@gmail.com>
2016-05-27 14:09:43 -1000

    default serve=T

--- R/labnb_serve.R
+++ R/labnb_serve.R
@@ -1,6 +1,6 @@
-labnb_serve <- function() {
+labnb_serve <- function(serve=T) {
   servr::jekyll(input = c(list.dirs("_source")),
                 output = c(rep("_posts", length(list.dirs("_source")))),
-                serve = TRUE)
+                serve = serve)
 }
 
