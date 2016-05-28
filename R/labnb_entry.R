62f3b094cec6c0d9ccdba8683f75518afb880f5a
Ross Cunning <ross.cunning@gmail.com>
2016-05-27 14:02:23 -1000

    update

--- R/labnb_entry.R
+++ R/labnb_entry.R
@@ -16,22 +16,13 @@ labnb_entry <- function(title = "new post", serve = FALSE, dir = "_source", subd
   }
 
   rmd_name <- file.path(fpath, paste0(Sys.Date(), "-", fname, ".Rmd"))
-  #r_name   <- file.path(fpath, paste0(fname, ".R"))
 
   # Read in the skeleton post
-  # If it doesn't exist, emit a warning and use the package default
   post <- readLines("skeleton_post.Rmd")
   post[grepl("title: ", post)] <- paste0("title:  ", title)
   writeLines(post, rmd_name)
 
-  # Write out an empty R file as well, in case that's useful
-  #writeLines(
-  #  c("# This R file accomanies the .Rmd blog post", paste("#", rmd_name), ""),
-  #  r_name
-  #)
-
-  #sys_open(r_name)
-  sys_open(rmd_name)
+  system(paste("open", rmd_name, sep=" "))
 
   if(serve) {
     labnb_serve(serve=T)
