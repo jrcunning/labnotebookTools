639906d70f4e9c74b3c8272a5b62f59fdcaf4977
Ross Cunning <ross.cunning@gmail.com>
2017-03-03 14:47:11 -0500

    do not print table, just return

--- R/qPCRlayout.R
+++ R/qPCRlayout.R
@@ -8,9 +8,8 @@ qPCRlayout <- function(samples=1:24, targets=c("Mc", "CD"), start=c(1,1)) {
   treps <- c(rep(NA, blanks), treps)
   if (length(treps) > 96) treps <- treps[1:96]
   t <- matrix(treps, nrow=8, byrow=T)
-  df <- data.frame(matrix(paste(t(s), '\\', t(t)), nrow=8, byrow=T))
+  df <- data.frame(matrix(paste(t(t), '/', t(s)), nrow=8, byrow=T))
   colnames(df) <- 1:12
   rownames(df) <- LETTERS[1:8]
-  knitr::kable(df)
   return(df)
 }
